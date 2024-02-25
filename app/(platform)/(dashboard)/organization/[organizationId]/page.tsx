533a0c550073a73cbd368a35e60e19f5c938d610
esakat <esaka.tom@gmail.com>
2024-02-25 13:54:37 +0900

    Server Actions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,20 @@
-import { OrganizationSwitcher, auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { Board } from "./board";
+import { Form } from "./form";
 
-const organizationIdPage = () => {
-  return <div>Organization Page!</div>;
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
+  return (
+    <div>
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} title={board.title} id={board.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default organizationIdPage;
+export default OrganizationIdPage;

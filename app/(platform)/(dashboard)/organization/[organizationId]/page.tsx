bff5e708dd942d36bc203b557c50b34174055021
esakat <esaka.tom@gmail.com>
2024-04-02 10:49:33 +0900

    Board Popover Form

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,17 +1,17 @@
 import { db } from "@/lib/db";
-import { Board } from "./board";
-import { Form } from "./form";
+import { Info } from "./_components/info";
+import { Separator } from "@/components/ui/separator";
+import { BoardList } from "./_components/board-list";
 
 const OrganizationIdPage = async () => {
   const boards = await db.board.findMany();
 
   return (
-    <div>
-      <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board key={board.id} title={board.title} id={board.id} />
-        ))}
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
       </div>
     </div>
   );
